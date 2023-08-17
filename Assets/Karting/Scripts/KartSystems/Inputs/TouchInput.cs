442cef86ae2db8e53944290446a680ef60e462f0
Resident <bionqine@gmail.com>
2023-08-17 09:22:07 +0200

    init commit

--- Assets/Karting/Scripts/KartSystems/Inputs/TouchInput.cs
+++ Assets/Karting/Scripts/KartSystems/Inputs/TouchInput.cs
@@ -9,18 +9,25 @@ namespace KartGame.KartSystems {
         public string AccelerateButtonName = "Accelerate";
         public string BrakeButtonName = "Brake";
 
+<<<<<<< HEAD
         public Vector2 SwipeDirection;
 
         private void Awake() => SwipeInput.SwipeEvent += OnSwipe;
 
         private void OnSwipe(Vector2 direction) => SwipeDirection = direction;
 
+=======
+>>>>>>> f7bb07d (init commit)
         public override InputData GenerateInput() {
             return new InputData
             {
                 Accelerate = Convert.ToBoolean(Input.touchCount),
                 Brake = Input.GetButton(BrakeButtonName),
+<<<<<<< HEAD
                 TurnInput = SwipeDirection.x
+=======
+                TurnInput = Input.GetAxis("Horizontal")
+>>>>>>> f7bb07d (init commit)
             };
         }
     }
